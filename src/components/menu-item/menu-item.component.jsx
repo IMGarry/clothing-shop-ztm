bbd96f76e8ddbf61970b5b44f2188fc88e109ad0
Igor Marchenko <igor.ms83@gmail.com>
2021-12-06 23:52:18 +0300

    add new features

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./menu-item.component.scss"
+
+const MenuItem = ({ title, imageUrl, size }) => (
+
+        <div 
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        className={`${size} menu-item`}>
+            <div className="content">
+                <h2 className="">{title}</h2>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+
+)
+
+
+export default MenuItem
