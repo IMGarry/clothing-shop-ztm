9087b3c7f45ca59860cf48307fc743c10420f2d7
Igor Marchenko <igor.ms83@gmail.com>
2022-02-24 00:49:14 +0300

    added button to item-component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CustomButton from '../custom-button/custom-button.component'
 import './collection-item.styles.scss'
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
@@ -13,9 +14,8 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div className="collection-footer">
             <span className="name">{name}</span>
             <span className="price">{price}</span>
-
         </div>
-
+        <CustomButton inverted>ADD TO CART</CustomButton>
     </div>
 )
 
